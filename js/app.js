b8c513b7edfb531ae4539203ba8aa08b7913c468
dalazaro <daryl.lazaro@gmail.com>
2017-07-02 17:13:19 -0700

    created turn sequence and marking the board

--- js/app.js
+++ js/app.js
@@ -4,6 +4,32 @@ $(document).ready(function() {
     // all code to manipulate the DOM
     // goes inside this function
 
-    
+    //initialize moves counter to 0
+    let moves = 0;
+
+    //X goes first
+    let whoseTurn = "X";
+
+    //function that switches to the next player's turn
+    let changeTurn = function() {
+      if (whoseTurn === "X") {
+        whoseTurn = "O";
+      } else {
+        whoseTurn = "X";
+      }
+    };
+
+    //when a box is clicked, it returns an X or O to the chosen box based on whose turn it is
+    $('.box').click(function(){
+      $(this).html('<span class="marker">'+whoseTurn+'</span>');
+      changeTurn();
+    });
+
+
+
+
+
+
+
 
 });
